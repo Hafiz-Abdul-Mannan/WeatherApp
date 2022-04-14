758dec980984efe228ca04e92403400f12e83058
Hafiz-Abdul-Mannan <hafiz.abdul.mannan@outlook.com>
2022-04-14 22:18:42 +0200

    fifth commit

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,30 +12,36 @@ import { MainService } from './main.service';
 export class MainComponent implements OnInit {
   favoriteCities = ['Paderborn', 'Lahore', 'Karachi', 'Colonge', 'Islamabad'];
   weatherInCities: Weather[] = [];
+  weather = ['Sunny', 'Clouds', 'Rain'];
   width = 1000;
   height = 500;
-  cellsToShow = 3;
+  cellsToShow: number = 3;
   cellWidth = 250;
   arrowsOutside = true;
-  // i: number = 0;
-  // get image() {
-  //   return '../../assets/images/' + this.weatherInCities[this.i].weather + '.png';
-  // }
-  get zeeshan() {
-    return 150;
-  }
+  overflowCellsLimit = 3;
+  showSearchCard = false;
 
   constructor(private mainSevice: MainService, private postService: PostService) {
+    if (screen.width <= 450) {
+      this.cellsToShow = 1;
+      this.arrowsOutside = false;
+      this.overflowCellsLimit = 1;
 
+    }
+    this.width = 0.78 * screen.width;
   }
 
   ngOnInit(): void {
-    this.favoriteCities.forEach(city => {
-      this.postService.fetchPost(city).subscribe(res => this.weatherInCities.push(res));
-    })
-    console.log(screen.width);
+    this.gettingData();
   }
-  weather = ['Sunny', 'Clouds', 'Rain'];
+  gettingData() {
+    this.favoriteCities.forEach(city => {
+      this.fetcherData(city);
+    })
+  }
+  fetcherData(name: string) {
+    this.postService.fetchPost(name).subscribe(res => this.weatherInCities.push(res));
+  }
 
   image(name: string) {
     if (this.weather.includes(name)) {
@@ -43,6 +49,13 @@ export class MainComponent implements OnInit {
     }
     return '../../assets/images/Sunny.png';
 
+  }
+  onAdd() {
+    this.showSearchCard = true;
+  }
+  onSearch(name: string) {
+    this.fetcherData(name);
+    this.showSearchCard = false;
   }
 
 }
